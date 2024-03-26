571aebebf1e1d9855b89443b71550dc7933dd304
jestrelloso <jestrelloso@jairosoftcom.onmicrosoft.com>
2024-03-26 09:32:43 +0800

    done CRUD for artist

--- routes/authuser_route.py
+++ routes/authuser_route.py
@@ -1,4 +1,5 @@
 import uuid
+from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
@@ -30,6 +31,95 @@ async def register_artist(
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="An error occured while creating the user!",
+        )
+
+
+# Route for getting all registered artists
+@router.get(
+    "/", status_code=status.HTTP_200_OK, response_model=List[gallery_schema.UserDisplay]
+)
+async def get_all_artists(db: Session = Depends(get_db)):
+    try:
+        users = db.query(gallery_model.AuthUser).all()
+        if users is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return users
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
+# Route for getting a single artist
+@router.get("/{user_id}", response_model=gallery_schema.UserDisplay)
+async def get_single_artist(user_id: str, db: Session = Depends(get_db)):
+    try:
+        user = (
+            db.query(gallery_model.AuthUser)
+            .filter(gallery_model.AuthUser.id == user_id)
+            .first()
+        )
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"User {user_id} not found!",
+            )
+        return user
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"User {user_id} not found!"
+        )
+
+
+# Route for updating
+@router.put("/{user_id}", status_code=status.HTTP_202_ACCEPTED)
+async def update_artist(
+    request: gallery_schema.AuthSchemaUpdate,
+    user_id: str,
+    db: Session = Depends(get_db),
+):
+    try:
+        user_query = db.query(gallery_model.AuthUser).filter(
+            gallery_model.AuthUser.id == user_id
+        )
+        if user_query.first() is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"User with id {user_id} not found",
+            )
+        user_query.update(
+            {
+                gallery_model.AuthUser.username: request.username,
+                gallery_model.AuthUser.password: Hash.bcrypt(request.password),
+            }
+        )
+        db.commit()
+        return {"Message": "User Updated", "User": user_query.first()}
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="An error occurred while updating the user!",
+        )
+
+
+# Route for deleting
+@router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_artist(user_id: str, db: Session = Depends(get_db)):
+    try:
+        user_query = db.query(gallery_model.AuthUser).filter(
+            gallery_model.AuthUser.id == user_id
+        )
+        user = user_query.first()
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"User with id {user_id} not found",
+            )
+        user_query.delete(synchronize_session=False)
+        db.commit()
+        return {"Status": "Success", "Message": "User deleted successfully!"}
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with an ID of {user_id} does not exist!",
         )
 
 
--- schemas/gallery_schema.py
+++ schemas/gallery_schema.py
@@ -1,10 +1,43 @@
 from datetime import datetime
+from typing import List
+from uuid import UUID
 
 from pydantic import BaseModel
 
 
+# ARTWORK
+class Artwork(BaseModel):
+    id: UUID
+    name: str
+    description: str
+    image: str
+
+    class Config:
+        orm_mode = True
+
+
+# USER Create schema
 class AuthSchema(BaseModel):
     username: str
     password: str
     createdAt: datetime | None = None
     updatedAt: datetime | None = None
+
+
+# USER Update schema
+class AuthSchemaUpdate(BaseModel):
+    username: str
+    password: str
+    updatedAt: datetime | None = None
+
+
+# USER Display schema
+class UserDisplay(BaseModel):
+    id: UUID
+    username: str
+    createdAt: datetime | None = None
+    updatedAt: datetime | None = None
+    artwork: List[Artwork] = []
+
+    class Config:
+        orm_mode = True
